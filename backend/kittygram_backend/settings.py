25be9e3a1ece4d4f7592b235d8fcf315e26ef938
TLS228 <solovei292000@gmail.com>
2024-10-14 09:58:47 +0000

    Добавлена папка infra с конфигурационными файлами

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -94,6 +94,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
